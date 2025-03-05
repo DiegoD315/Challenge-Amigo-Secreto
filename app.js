54d5ce3edfd7499af35b5fa5c14177179bd680ef
Diego Portalete <diegoportalete@hotmail.com>
2025-03-05 13:06:48 -0300

    Implementa uma função para agregar nomes à lista

--- app.js
+++ app.js
@@ -1,18 +1,30 @@
+let amigos = [];
+
 let listaAmigos= document.querySelector('ul');
-    listaAmigos.innerHTML= '';
+    listaAmigos.innerHTML= amigos;
+
+
 
 
 
 
 
 function adicionarAmigo(){
-    let amigos = document.querySelector('input').value;
+    let nomeInserido = document.querySelector('input').value;
 
-    if (amigos=='') {
-     alert ('O campo  está vazio');
+    if (nomeInserido=='') {
+     alert ('Por favor, insira um nome.');
+    } 
+    else {
+     amigos.push(nomeInserido);
+     console.log(amigos);
+     limparCampo();
     }
-    
-    
+    }
+
+function limparCampo() {
+        nomeInserido = document.querySelector('input');
+        nomeInserido.value= '';
     }
 
 

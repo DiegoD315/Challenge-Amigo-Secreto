05a4614c16d303e290fc4da0ef0038bd6fb26d97
Diego Portalete <diegoportalete@hotmail.com>
2025-03-05 18:03:34 -0300

    Implementa uma função para exibir os nomes na tela em lista, e verifica se já possui o nome inserido na lista.

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
 let amigos = [];
 
-let listaAmigos= document.querySelector('ul');
-    listaAmigos.innerHTML= amigos;
-
+function atualizaLista() {
+        
+        let lista = document.getElementById('listaAmigos');
+        lista.innerHTML = "";
+        
+        for (let i = 0; i < amigos.length; i++) {
+            let itemLista = document.createElement('li');
+            itemLista.innerText = amigos[i];
+            lista.appendChild(itemLista);
+        }
+        
+    }
 
 
 
@@ -12,20 +21,27 @@ let listaAmigos= document.querySelector('ul');
 function adicionarAmigo(){
     let nomeInserido = document.querySelector('input').value;
 
-    if (nomeInserido=='') {
+    if (nomeInserido=='' ) {
      alert ('Por favor, insira um nome.');
     } 
-    else {
+    else { if (amigos.includes(nomeInserido)){
+        alert ('Esse amigo já está na lista, insira outro amigo.')
+    } else {
      amigos.push(nomeInserido);
      console.log(amigos);
      limparCampo();
+     atualizaLista();
     }
     }
+}
 
 function limparCampo() {
         nomeInserido = document.querySelector('input');
         nomeInserido.value= '';
     }
+
+
+    
 
 
 
